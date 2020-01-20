137c07a0d2834486648735cf268da360cd471c91
= <samw430@gmail.com>
2020-01-20 16:37:49 -0500

    Cleaned up server code

--- server.js
+++ server.js
@@ -4,29 +4,30 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var path = require('path');
 
+//Makes index.html default page
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
+//Allows host to access past session names when recalling a session
 app.get('/session_names', function(req, res) {
 	res.setHeader('Content-Type', 'application/json');
 	var files = fs.readdirSync(__dirname + '/quizzes/');
-	console.log(files);
+	console.log("Recalled past sessions");
 	res.end(JSON.stringify({ "sessions": files }));
 });
 
+//End point for user to get the questions for the current session
 app.get('/session_questions', function(req, res){
 	var session_name = req.query.session;
 	console.log("Requested questions from " + session_name);
 
 	var url_query = url.parse(req.url, true).query["session"];	
-	var quiz_contents;
 	try{
 		var file_path = "./quizzes/" + url_query;
 		console.log(file_path);
-		quiz_contents = fs.readFileSync(file_path);
+		var quiz_contents = fs.readFileSync(file_path);
 		quiz_contents = JSON.parse(quiz_contents);
 		res.setHeader('Content-Type', 'application/json');
 		res.end(JSON.stringify({ "questions": quiz_contents["Questions"] }));
@@ -37,16 +38,16 @@ app.get('/session_questions', function(req, res){
 
 });
 
+//End point for user to get the current question for their session
 app.get('/current_question', function (req, res){
 	var session_name = req.query.session;
 	console.log("Requested current question from " + session_name);
 
 	var url_query = url.parse(req.url, true).query["session"];
-	var quiz_contents;
 	try {
 		var file_path = "./quizzes/" + url_query;
 		console.log(file_path);
-		quiz_contents = fs.readFileSync(file_path);
+		var quiz_contents = fs.readFileSync(file_path);
 		quiz_contents = JSON.parse(quiz_contents);
 		res.setHeader('Content-Type', 'application/json');
 		var currentQIndex = quiz_contents["Current Question"];
@@ -57,10 +58,12 @@ app.get('/current_question', function (req, res){
 	}
 });
 
+//Allow for html files to be accessed through IPAddress/desired-filename
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
+//End point for host to post session when it is complete
 app.post('/session_save', function(req, res) {
 	var url_query = url.parse(req.url, true).query["session"];
 
@@ -69,8 +72,10 @@ app.post('/session_save', function(req, res) {
 		stream.write(JSON.stringify(req.body));
 		stream.end();
 	});
+	res.sendStatus(200);
 });
 
+//End point for participants to post answers to live questions
 app.post('/submit_answer', function(req, res){
 	var session_nickname = req.body["Session"];
 	var user_nickname = req.body["User"];
@@ -81,22 +86,22 @@ app.post('/submit_answer', function(req, res){
 		console.log("Recieved answer for " + file_path);
 		quiz_contents = fs.readFileSync(file_path);
 		quiz_contents = JSON.parse(quiz_contents);
-		answers = quiz_contents["Answers"];
-		answers[user_nickname] = answer;
-		quiz_contents["Answers"] = answers;
-		res.sendStatus(200);
+
+		quiz_contents["Answers"][user_nickname] = answer;
 
 		var stream = fs.createWriteStream(file_path);
 		stream.once("open", function(fd) {
 			stream.write(JSON.stringify(quiz_contents));
 			stream.end();
 		});
+		res.sendStatus(200);
 	} catch (err) {
 		console.log("Invalid quiz requested");
 		res.sendStatus(404);
 	}
 });
 
+//End point for host to set the current live question
 app.post('/set_current_question', function(req, res){
 	var url_query = url.parse(req.url, true);
 	var session_nickname = url_query.query["session"];
@@ -108,19 +113,20 @@ app.post('/set_current_question', function(req, res){
 		quiz_contents = fs.readFileSync(file_path);
 		quiz_contents = JSON.parse(quiz_contents);
 		quiz_contents["Current Question"] = current_question;
-		res.sendStatus(200);
 
 		var stream = fs.createWriteStream(file_path);
 		stream.once("open", function(fd) {
 			stream.write(JSON.stringify(quiz_contents));
 			stream.end();
 		});
+		res.sendStatus(200);
 	} catch (err) {
 		console.log("Invalid quiz requested");
 		res.sendStatus(404);
 	}
 });
 
+//Starts node server on port 8080
 app.listen(8080, function(){
 	console.log("Started on PORT 8080");
 });
