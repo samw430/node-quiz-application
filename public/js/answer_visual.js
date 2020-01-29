bec765c8cc550aecdcdb5ab2cd245449af33d567
= <samw430@gmail.com>
2020-01-28 20:47:57 -0500

    Added d3.js visualization of results

--- public/js/answer_visual.js
+++ public/js/answer_visual.js
@@ -12,9 +12,9 @@ xhttp.onreadystatechange = function() {
 		var stored_answers = JSON.parse(this.responseText)["answers"];
 		var answersList = document.getElementById("answers");
 
-		console.log(this.responseText);
 		var keys = Object.keys(stored_answers);
-		console.log(keys);
+		var frequency_table = {};
+
 		for (var i = 0; i < keys.length; i++) {
 			var user_nickname = keys[i];
 			var answer = stored_answers[user_nickname];
@@ -25,7 +25,67 @@ xhttp.onreadystatechange = function() {
 			item.appendChild(header);
 
 			answersList.appendChild(item);
+
+			if(!(answer in frequency_table)){
+				frequency_table[answer] = 0;
+			}
+			frequency_table[answer] = frequency_table[answer] + 1;
 		}
+
+		data = [];
+		var keys = Object.keys(frequency_table);
+
+		for (var i = 0; i < keys.length; i++) {
+			var answer = keys[i];
+			var count = frequency_table[answer];
+
+			data.push({ "answer": answer, "frequency": count });
+		}
+
+		// set the dimensions and margins of the graph
+		var margin = {top: 20, right: 20, bottom: 30, left: 40},
+			width = 960 - margin.left - margin.right,
+			height = 500 - margin.top - margin.bottom;
+		
+		// set the ranges
+		var x = d3.scaleBand()
+				  .range([0, width])
+				  .padding(0.1);
+		var y = d3.scaleLinear()
+				  .range([height, 0]);
+		//append the svg object to the body of the page
+		// append a 'group' element to 'svg'
+		// moves the 'group' element to the top left margin
+		var svg = d3.select("body").append("svg").lower()
+		.attr("class", "graph-svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+				
+		// Scale the range of the data in the domains
+		x.domain(data.map(function(d) { return d.answer; }));
+	 	y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+				  
+		// append the rectangles for the bar chart
+		svg.selectAll(".bar").data(data)
+		.enter().append("rect").attr("fill", "steelblue")
+		.attr("class", "bar")
+	    .attr("x", function(d) { return x(d.answer); })
+		.attr("width", x.bandwidth())
+	    .attr("y", function(d) { return y(d.frequency); })
+	    .attr("height", function(d) { return height - y(d.frequency); });
+  
+		// add the x Axis
+		svg.append("g")
+		.attr("transform", "translate(0," + height + ")")
+		.call(d3.axisBottom(x));
+  
+		// add the y Axis
+		svg.append("g")
+		.call(d3.axisLeft(y));
+	  
+	  
 	}
 };
 xhttp.open("GET", "question_answers?session=" + sessionNickname + "&question=" + questionNumber, true);
